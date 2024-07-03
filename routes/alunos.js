494e157cdb7dbdb0a21dd3108ff849f6ae82ae55
kinauanne <kinauannemenezes@gmail.com>
2024-07-03 17:10:44 -0300

    Primeiro commit

--- routes/alunos.js
+++ routes/alunos.js
@@ -3,7 +3,7 @@ const router = express.Router();
 let alunos = require('../tests/mocks/alunos.json');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(_req, res, next) {
   const data = {
     title: 'Alunos',
     alunos: alunos.content
@@ -11,12 +11,16 @@ router.get('/', function(req, res, next) {
   res.render('list', data);
 });
 
-module.exports = router;
+router.get('/new', function (_req, res, next) {
+  res.render('form', { title: ' Novo aluno', buttonText: 'Adicionar'});
+});
 
 router.get('/:matricula', function (req, res, next) {
   const { matricula } = req.params;
 
   const aluno = alunos.content[matricula];
 
-  res.render('card', { title: 'Detalhes do aluno', aluno})
+  res.render('card', { title: 'Detalhes do aluno', aluno});
 });
+
+module.exports = router;
