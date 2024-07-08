2d03accc7d3f2a9ac9e910bda10dc3b23d33dd60
kinauanne <kinauannemenezes@gmail.com>
2024-07-08 17:03:49 -0300

    as

--- routes/alunos.js
+++ routes/alunos.js
@@ -1,54 +1,68 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 let alunos = require('../tests/mocks/alunos.json');
-router.get('/', function(req, res, next) {
-    const data = {
-        title: 'Alunos',
-        alunos: alunos.content
-    }
-    res.render('all_students',data);
+
+/* GET users listing. */
+router.get('/', function(_req, res, next) {
+  const data = {
+    title: 'Alunos',
+    alunos: alunos.content
+  }
+  res.render('list', data);
 });
-router.get('/new', function(req, res, next) {
-    parametro = "create"
-    const data= {metodo:"POST",parametro, title: 'Novo Aluno',buttonText:'Adicionar Aluno'}
-    res.render('form',data);
+
+router.get('/new', function (_req, res, next) {
+  const { heads: labels } = alunos
+  const parametro = "create";
+  const data = { metodo: "post", parametro, title: 'Novo aluno', buttonText: "adicionar"}
+  res.render('form', data);
 });
-router.post('/', function (req, res, next) {
-    const{body,method} = req
-    res.send({body,method});
+
+router.post('/', function (req, res, next) { 
+  const { body, method} = req;
+  
+  res.send({ body, method});
 });
-router.post('/create',function(req,res,next){
-    let novoAluno = req.body
-    let matricula = novoAluno.matricula
-    alunos.content[matricula] = {
-        ...novoAluno,
-        matricula:Number(matricula)
-    }
-    res.redirect('/alunos')
-})
+
+
+router.post('/create', function (req, res, next) {
+  let novoAluno = req.body;
+  let matricula = novoAluno.matricula;
+
+  alunos.content[matricula] = {
+    ...novoAluno,
+    matricula: Number(matricula)
+  }
+  res.redirect('/alunos');
+});
+
 router.get('/:matricula', function(req,res,next){
-    const{matricula} = req.params;
-    const aluno = alunos.content[matricula];
-    res.render('details_student',{title:'Detalhes do Aluno',aluno})
+  const{matricula} = req.params;
+  const aluno = alunos.content[matricula];
+  res.render('card',{title:'Detalhes do Aluno',aluno})
 })
-router.get('/edit/:matricula', function(req, res, next) {
-    const{matricula} = req.params;
-    const aluno = alunos.content[matricula];
-    const parametro = matricula;
-    res.render('form',{metodo: "PUT",parametro, title: 'Editar Aluno',buttonText:'Salvar Alterações',aluno});
+
+router.get('/edit/:matricula', function (req, res, next) {
+  const { matricula } = req.params;
+  const parametro = matricula;
+  const aluno = alunos.content[matricula];
+
+  res.render('form', {metodo: "put", parametro, title: ' Editar aluno', buttonText: 'Salvar alterações', aluno});
 });
+
 router.put('/:matricula', function (req, res, next) {
-    // const{body,method} = req
-    const {matricula} = req.params;
-    const novoAluno = req.body;
-    alunos.content[matricula] = {
-        ...novoAluno,
-        matricula:Number(matricula)};
-    res.redirect('/alunos')
-    // res.send({body,method,msg:'Alterar o aluno'} );
+  // const{body,method} = req
+  const {matricula} = req.params;
+  const novoAluno = req.body;
+  alunos.content[matricula] = {
+      ...novoAluno,
+      matricula:Number(matricula)};
+  res.redirect('/alunos')
+  // res.send({body,method,msg:'Alterar o aluno'} );
 });
-router.delete('/', function (req, res, next) {
-    const{body,method} = req
-    res.send({body,method,msg:'Vai remover aluno'});
+router.delete('/:matricula', function (req, res, next) {
+  const matricula = req.params.matricula;
+  delete alunos.content[matricula]
+  res.redirect(303, '/alunos')
 });
 module.exports = router;
